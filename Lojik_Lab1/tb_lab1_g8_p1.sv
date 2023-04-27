63e464b7631e64df8f163228a1d64f1dc3943f43
Esra Kirman <e.kirman2020@gtu.edu.tr>
2023-04-27 23:30:04 +0300

    ss

--- Lojik_Lab1/tb_lab1_g8_p1.sv
+++ Lojik_Lab1/tb_lab1_g8_p1.sv
@@ -1,30 +1,31 @@
-module tb_lab1_g8_p1 ( 
-	logic G, T, U, E,
-	logic Y
-);
+module tb_lab1_g8_p1 ();
+    logic g_tb, t_tb, u_tb, e_tb;
+    logic y_tb;
 
-lab1_g8_p1 dut0 (G, T, U, E, Y); 
+
+lab1_g8_p1 dut0(g_tb, t_tb, u_tb, e_tb, y_tb); 
 
 initial begin
 
-	G = 0; T = 0; U = 0; E = 0; 		#10
-	E = 1;					#10
-	U = 1; E = 0;				#10
-	E = 1;					#10
-	T = 1; U = 0; E = 0;			#10
-	E = 1;					#10
-	U = 1; E = 0;				#10
-	E = 1;					#10
-	G = 1; T = 0; U = 0; E = 0;		#10
-	E = 1;					#10
-	U = 1; E = 0;				#10
-	E = 1;					#10
-	T = 1; U = 0; E = 0;			#10
-	E = 1;					#10
-	U = 1; E = 0;				#10
-	G = 1; T = 1; U = 1; E = 1;		#10
+    g_tb = 0; t_tb = 0; u_tb = 0; e_tb = 0;        	 #10
+    e_tb = 1;                  				 #10
+    u_tb = 1; e_tb = 0;                			 #10
+    e_tb = 1;                  				 #10
+    t_tb = 1; u_tb = 0; e_tb = 0;        		 #10
+    e_tb = 1;                  				 #10
+    u_tb = 1; e_tb = 0;              			 #10
+    e_tb = 1;                   			 #10
+    g_tb = 1; t_tb = 0; u_tb = 0; e_tb = 0;              #10
+    e_tb = 1;                    		 	 #10
+    u_tb = 1; e_tb = 0;               	    		 #10
+    e_tb = 1;                   		         #10
+    t_tb = 1; u_tb = 0; e_tb = 0;           	         #10
+    e_tb = 1;                   		         #10
+    u_tb = 1; e_tb = 0;               	                 #10
+    g_tb = 1; t_tb = 1; u_tb = 1; e_tb = 1;              #10
 
-	$stop;
-end
+    $stop;
+end
+
 
 endmodule

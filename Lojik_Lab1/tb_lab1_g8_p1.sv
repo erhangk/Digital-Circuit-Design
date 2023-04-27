b100b084a54e59c8faaf1592b70b8d31c37c6fba
Esra Kirman <e.kirman2020@gtu.edu.tr>
2023-04-27 23:24:37 +0300

    sd

--- Lojik_Lab1/tb_lab1_g8_p1.sv
+++ Lojik_Lab1/tb_lab1_g8_p1.sv
@@ -1,31 +1,30 @@
-module tb_lab1_g8_p1 ();
-    logic g, t, u, e;
-    logic y;
+module tb_lab1_g8_p1 ( 
+	logic G, T, U, E,
+	logic Y
+);
 
-
-lab1_g8_p1 dut0(g, t, u, e, y); 
+lab1_g8_p1 dut0 (G, T, U, E, Y); 
 
 initial begin
 
-    g = 0; t = 0; u = 0; e = 0;        	 #10
-    e = 1;                  		 #10
-    u = 1; e = 0;                	 #10
-    e = 1;                  		 #10
-    t = 1; u = 0; e = 0;        	 #10
-    e = 1;                  		 #10
-    u = 1; e = 0;              		 #10
-    e = 1;                   		 #10
-    g = 1; t = 0; u = 0; e = 0;          #10
-    e = 1;                    		 #10
-    u = 1; e = 0;               	 #10
-    e = 1;                   		 #10
-    t = 1; u = 0; e = 0;           	 #10
-    e = 1;                   		 #10
-    u = 1; e = 0;               	 #10
-    g = 1; t = 1; u = 1; e = 1;          #10
+	G = 0; T = 0; U = 0; E = 0; 		#10
+	E = 1;					#10
+	U = 1; E = 0;				#10
+	E = 1;					#10
+	T = 1; U = 0; E = 0;			#10
+	E = 1;					#10
+	U = 1; E = 0;				#10
+	E = 1;					#10
+	G = 1; T = 0; U = 0; E = 0;		#10
+	E = 1;					#10
+	U = 1; E = 0;				#10
+	E = 1;					#10
+	T = 1; U = 0; E = 0;			#10
+	E = 1;					#10
+	U = 1; E = 0;				#10
+	G = 1; T = 1; U = 1; E = 1;		#10
 
-    $stop;
-end
-
+	$stop;
+end
 
 endmodule
